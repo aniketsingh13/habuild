daa2e46af75505d9d8f179aba2d8eb4375f37792
aniketsingh13 <85512784+aniketsingh13@users.noreply.github.com>
2022-11-13 20:01:58 +0530

    feat: edit todo

--- assignmentss/src/Pages/Todo/Todo.jsx
+++ assignmentss/src/Pages/Todo/Todo.jsx
@@ -9,14 +9,39 @@ let nextId = 1;
 const Todo = () => {
   const [data, setData] = useState([]);
   const [input, setInput] = useState("");
+  const [isedit, setIsEdit] = useState(false);
+  const [newid, setNewid] = useState(null);
 
   function addData() {
-    setData([...data, { id: nextId++, input: input }]);
-    setInput("");
+    if (!input) {
+      alert("please fill input field");
+    } else if (input && isedit) {
+      setData(
+        data.map((item) => {
+          if (item.id === newid) {
+            return { ...item, input: input };
+          }
+          return item;
+        })
+      );
+      setInput("");
+      setIsEdit(false);
+      setNewid(null);
+    } else {
+      setData([...data, { id: nextId++, input: input }]);
+      setInput("");
+    }
   }
 
-  function deleteTodo(ID){
-     setData(data.filter(item => item.id !== ID))
+  function deleteTodo(ID) {
+    setData(data.filter((item) => item.id !== ID));
+  }
+
+  function editTodo(ID) {
+    let newInput = data.find((item) => item.id === ID);
+    setIsEdit(true);
+    setInput(newInput.input);
+    setNewid(ID);
   }
 
   return (
@@ -35,11 +60,16 @@ const Todo = () => {
           <input
             type="text"
             className="todo_input"
+            placeholder="✍️ Add item"
             value={input}
             onChange={(e) => setInput(e.target.value)}
           />
           <span className="todo_button" onClick={addData}>
-            <AiOutlinePlus className="todo_addIcon" />
+            {isedit ? (
+              <FaEdit className="todo_addIcon" />
+            ) : (
+              <AiOutlinePlus className="todo_addIcon" />
+            )}
           </span>
         </div>
         <div>
@@ -47,10 +77,13 @@ const Todo = () => {
             <div className="todo_data" key={item.id}>
               <span className="todo_inputs">{item.input}</span>
               <span className="todo_editDelete">
-                <span className="todo_delete" onClick={() => deleteTodo(item.id)}>
+                <span
+                  className="todo_delete"
+                  onClick={() => deleteTodo(item.id)}
+                >
                   <AiOutlineDelete />
                 </span>
-                <span>
+                <span className="todo_edit" onClick={() => editTodo(item.id)}>
                   <FaEdit />
                 </span>
               </span>
